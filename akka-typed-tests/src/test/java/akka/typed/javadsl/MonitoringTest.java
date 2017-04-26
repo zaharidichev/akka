ad910c59b0e9091871918fd2cc6db5aeea7eb9d3
Arnout Engelen <arnouten@bzzt.net>
2017-04-26 12:49:36 +0200

    Implement watchWith to customize the termination msg (#18778)

--- akka-typed-tests/src/test/java/akka/typed/javadsl/MonitoringTest.java
+++ akka-typed-tests/src/test/java/akka/typed/javadsl/MonitoringTest.java
@@ -20,13 +20,15 @@ import akka.testkit.AkkaSpec;
 
 public class MonitoringTest extends JUnitSuite {
 
-    static final class RunTest<T> {
+    static interface Message {}
+    static final class RunTest<T> implements Message {
         private final ActorRef<T> replyTo;
         public RunTest(ActorRef<T> replyTo) {
             this.replyTo = replyTo;
         }
     }
     static final class Stop {}
+    static final class CustomTerminationMessage implements Message {}
 
     // final FiniteDuration fiveSeconds = FiniteDuration.create(5, TimeUnit.SECONDS);
     final Timeout timeout = new Timeout(Duration.create(5, TimeUnit.SECONDS));
@@ -48,6 +50,18 @@ public class MonitoringTest extends JUnitSuite {
         );
     }
 
+    private Behavior<Message> waitingForMessage(ActorRef<Done> replyWhenReceived) {
+        return immutable(
+            (ctx, msg) -> {
+                if (msg instanceof CustomTerminationMessage) {
+                    replyWhenReceived.tell(Done.getInstance());
+                    return same();
+                } else {
+                    return unhandled();
+                }
+            }
+        );
+    }
 
     @Test
     public void shouldWatchTerminatingActor() throws Exception {
@@ -62,6 +76,26 @@ public class MonitoringTest extends JUnitSuite {
         // Not sure why this does not compile without an explicit cast?
         // system.tell(new RunTest());
         CompletionStage<Done> result = AskPattern.ask((ActorRef<RunTest<Done>>)system, (ActorRef<Done> ref) -> new RunTest<Done>(ref), timeout, system.scheduler());
+        result.toCompletableFuture().get(3, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void shouldWatchWithCustomMessage() throws Exception {
+        Behavior<Message> root = immutable((ctx, msg) -> {
+            if (msg instanceof RunTest) {
+                ActorRef<Stop> watched = ctx.spawn(exitingActor, "exitingActor");
+                ctx.watchWith(watched, new CustomTerminationMessage());
+                watched.tell(new Stop());
+                return waitingForMessage(((RunTest) msg).replyTo);
+            } else {
+                return unhandled();
+            }
+        });
+        ActorSystem<Message> system = ActorSystem$.MODULE$.create("sysname", root, Optional.empty(), Optional.empty(), Optional.empty(), Optional.empty());
+
+        // Not sure why this does not compile without an explicit cast?
+        // system.tell(new RunTest());
+        CompletionStage<Done> result = AskPattern.ask((ActorRef<Message>)system, (ActorRef<Done> ref) -> new RunTest<Done>(ref), timeout, system.scheduler());
         result.toCompletableFuture().get(3, TimeUnit.SECONDS);
     }
 }
